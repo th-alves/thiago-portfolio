74f88650c09d91b6e23cdaec387d7bb96ab3f179
Thiago Souza <thiagosouza.contato01@gmail.com>
2024-02-08 09:59:46 -0300

    Create FrontEnd Mentor projects

---
+++ assets/images/projects-image/Frontend-Mentor/four-card/a.js
@@ -0,0 +1,7 @@
+// Solicita ao usuário que insira o nome da equipe
+const nomeDaEquipe = "Os Lutadores";
+
+// Imprime etiquetas para os 5 membros da equipe
+for (let i = 1; i <= 5; i++) {
+    console.log(nomeDaEquipe + " - " + i);
+}
